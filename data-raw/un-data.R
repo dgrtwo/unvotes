1b776b786bda26b393628a3ffaae78b6d0324cc7
ngoguened <s2013017@ed.ac.uk>
2021-03-04 16:10:15 +0000

    Updated data prep code.

--- data-raw/un-data.R
+++ data-raw/un-data.R
@@ -2,46 +2,60 @@ library(dplyr)
 library(readxl)
 library(countrycode)
 library(tidyr)
-library(devtools)
+library(forcats)
+library(usethis)
 
 vlevels <- c("yes", "abstain", "no")
 
-load("data-raw/UNVotesPublished.RData")
-un_votes <- x %>%
-  tbl_df() %>%
+load("data-raw/UNVotes2021.RData")
+
+un_votes <- completeVotes %>%
   filter(vote <= 3) %>%
-  mutate(country = countrycode(ccode, "cown", "country.name"),
-         country_code = countrycode(ccode, "cown", "iso2c")) %>%
+  mutate(
+    country = countrycode(ccode, "cown", "country.name"),
+    country_code = countrycode(ccode, "cown", "iso2c"),
+    # Match based on old version of data from unvotes package
+    country_code = case_when(
+      country == "Czechoslovakia" ~ "CS",
+      country == "Yugoslavia" ~ "YU",
+      country == "German Democratic Republic" ~ "DD",
+      country == "Yemen People's Republic" ~ "YD",
+      TRUE ~ country_code
+    ),
+    country = if_else(Countryname == "German Federal Republic", "Federal Republic of Germany", country)
+  ) %>%
   select(rcid, country, country_code, vote) %>%
-  mutate(vote = factor(vlevels[vote], levels = vlevels))
+  mutate(vote = as.character(vote)) %>%
+  mutate(vote = fct_recode(vote, yes = "1", abstain = "2", no = "3"))
 
-devtools::use_data(un_votes, overwrite = TRUE)
+usethis::use_data(un_votes, overwrite = TRUE)
 
-descriptions_raw <- read_excel("data-raw/descriptions1-70latestversion.xls")
-# transcription error in Excel
-descriptions_raw$ec[90] <- 0
+descriptions <- completeVotes %>%
+  select(session, rcid, abstain, yes, no, importantvote, date, unres, amend, para, short, descr, me, nu, di, hr, co, ec) %>%
+  distinct(rcid, .keep_all = TRUE)
 
-un_roll_calls <- descriptions_raw %>%
+un_roll_calls <- descriptions %>%
   select(rcid, session, importantvote:descr) %>%
   mutate(rcid = as.integer(rcid),
          date = as.Date(date)) %>%
   arrange(rcid)
 
-devtools::use_data(un_roll_calls, overwrite = TRUE)
+usethis::use_data(un_roll_calls, overwrite = TRUE)
 
-un_roll_call_issues <- descriptions_raw %>%
+un_roll_call_issues <- descriptions %>%
   select(rcid, me:ec) %>%
   gather(short_name, value, me:ec) %>%
   mutate(rcid = as.integer(rcid),
          value = as.numeric(value)) %>%
   filter(value == 1) %>%
   select(-value) %>%
-  mutate(issue = plyr::revalue(short_name,
-                               c(me = "Palestinian conflict",
-                                 nu = "Nuclear weapons and nuclear material",
-                                 co = "Colonialism",
-                                 hr = "Human rights",
-                                 ec = "Economic development",
-                                 di = "Arms control and disarmament")))
+  mutate(issue = fct_recode(short_name,
+                            "Palestinian conflict" = "me",
+                            "Nuclear weapons and nuclear material" = "nu",
+                            "Colonialism" = "co",
+                            "Human rights" = "hr",
+                            "Economic development" = "ec",
+                            "Arms control and disarmament" = "di"))
 
-devtools::use_data(un_roll_call_issues, overwrite = TRUE)
+usethis::use_data(un_roll_call_issues, overwrite = TRUE)
+
